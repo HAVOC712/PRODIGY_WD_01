1dbb7a10ef41843cbedfe418e4c952f2806f665b
Sarvesh <sarveshnagawade@gmail.com>
2026-03-31 15:01:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// SELECT NAVBAR
+const navbar = document.getElementById("navbar");
+
+// SCROLL EVENT
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
